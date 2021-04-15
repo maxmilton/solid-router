5b3b9c787c13a2d26d45925a734bff5185072b51
Max Milton <max@wearegenki.com>
2021-04-15 13:51:38 +1000

    Put placeholders for tests

---
+++ test/NavLink.test.tsx
@@ -0,0 +1,38 @@
+export {};
+// // FIXME: JSDOM doesn't support navigation making testing a router rather difficult
+
+// import { cleanup, render } from 'solid-testing-library';
+// import { NavLink } from '../src';
+
+// afterEach(cleanup);
+
+// test('renders without props', () => {
+//   expect.assertions(1);
+//   // @ts-expect-error - Missing props are intentional
+//   const rendered = render(() => <NavLink />);
+//   expect(rendered.container.innerHTML).toMatchInlineSnapshot('"<a></a>"');
+// });
+
+// test('renders correctly with required props', () => {
+//   expect.assertions(1);
+//   const rendered = render(() => <NavLink href="">x</NavLink>);
+//   expect(rendered.container.innerHTML).toMatchInlineSnapshot(
+//     '"<a href=\\"\\">x</a>"',
+//   );
+// });
+
+// test.todo('renders "aria-current" attribute when location matches');
+// test.todo(
+//   'does not render "aria-current" attribute when location does not match',
+// );
+// test.todo('renders "aria-current" attribute only on matching links');
+// test.todo('renders "aria-current" attribute when location deep matches');
+// test.todo(
+//   'does not render "aria-current" attribute when location does not deep match',
+// );
+// test.todo(
+//   'renders "aria-current" attribute after changing to a matching location',
+// );
+// test.todo('adds props as attributes on <a> element');
+// test.todo('does not add deepMatch prop as attribute on <a> element');
+// test.todo('');
---
+++ test/routeTo.test.ts
@@ -0,0 +1,12 @@
+export {};
+// // FIXME: JSDOM doesn't support navigation making testing a router rather difficult
+
+// // import { routeTo } from '../src';
+
+// test.todo('navigates to new route adding a new history entry');
+// test.todo('navigates to new route replacing current history entry');
+// test.todo('navigates to route within the current location host');
+// test.todo('navigates to route outside of the current location host');
+// test.todo('updates location with URL search query params');
+// test.todo('updates location with hash');
+// test.todo('updates location with URL search query params and hash');
