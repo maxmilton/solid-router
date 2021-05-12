ed89a0ff149c863161b748908112064be8272e6e
Max Milton <max@wearegenki.com>
2021-05-12 19:11:12 +1000

    Better lint tooling configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
 const OFF = 0;
 
-/** @type {import('eslint').Linter.Config} */
+// eslint-disable-next-line max-len
+/** @type {import('eslint').Linter.Config & { parserOptions: import('@typescript-eslint/types').ParserOptions }} */
 module.exports = {
   root: true,
   reportUnusedDisableDirectives: true,
   parser: '@typescript-eslint/parser',
   parserOptions: {
     extraFileExtensions: ['.mjs', '.cjs'],
-    project: './tsconfig.json',
-  },
-  env: {
-    node: true,
+    project: ['./test/tsconfig.json'],
   },
   extends: [
     'eslint:recommended',
@@ -24,12 +24,4 @@ module.exports = {
     // used safely in this project
     '@typescript-eslint/no-non-null-assertion': OFF,
   },
-  overrides: [
-    {
-      files: ['.eslintrc.js'],
-      parserOptions: {
-        createDefaultProgram: true,
-      },
-    },
-  ],
 };
