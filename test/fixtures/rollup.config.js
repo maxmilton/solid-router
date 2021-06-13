5ec82cd760a93159518dfb37852dd885ed46112c
Max Milton <max@wearegenki.com>
2021-06-13 14:23:48 +1000

    Build test fixture seperately

---
+++ test/fixtures/rollup.config.js
@@ -0,0 +1,52 @@
+/* eslint-disable import/no-extraneous-dependencies */
+
+import babel from '@rollup/plugin-babel';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import path from 'path';
+
+const fixtures = ['lazyload', 'simple'];
+const extensions = ['.ts', '.tsx', '.js', '.jsx', '.mjs', '.cjs'];
+const htmlTemplate = `<!doctype html>
+<meta charset=utf-8>
+<script src=index.js type=module defer></script>`;
+
+export default fixtures.map((name) => ({
+  input: path.join(__dirname, name, 'index.tsx'),
+  output: {
+    assetFileNames: '[name][extname]',
+    entryFileNames: '[name].js',
+    chunkFileNames: '[name].js',
+    dir: path.join(__dirname, 'dist', name),
+    format: 'esm',
+    sourcemap: true,
+  },
+  preserveEntrySignatures: false,
+  plugins: [
+    nodeResolve({
+      extensions,
+    }),
+    babel({
+      include: [/\.tsx?$/],
+      extensions,
+      babelrc: false,
+      babelHelpers: 'bundled',
+      presets: ['@babel/preset-typescript', 'babel-preset-solid'],
+      skipPreflightCheck: true,
+    }),
+    {
+      name: 'html',
+      generateBundle() {
+        this.emitFile({
+          fileName: 'index.html',
+          source: htmlTemplate,
+          type: 'asset',
+        });
+        this.emitFile({
+          fileName: 'favicon.ico',
+          source: '',
+          type: 'asset',
+        });
+      },
+    },
+  ],
+}));
