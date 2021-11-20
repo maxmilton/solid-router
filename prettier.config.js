0c14f9f80f0f023eb32d73dc1020c1d1ba3fcd61
Max Milton <max@wearegenki.com>
2021-11-20 11:24:15 +1000

    Move prettier config into seperate file

---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  endOfLine: 'lf',
+  singleQuote: true,
+  trailingComma: 'all',
+  plugins: [
+    './node_modules/prettier-plugin-pkg',
+    './node_modules/prettier-plugin-sh',
+  ],
+};
