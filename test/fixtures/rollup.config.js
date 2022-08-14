0472d9d83912686fb57ae62d1fb72315b6eda7b3
Max Milton <max@happycode.ltd>
2022-08-14 12:06:23 +1000

    Remove unused lint ignore comment

--- test/fixtures/rollup.config.js
+++ test/fixtures/rollup.config.js
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
 import babel from '@rollup/plugin-babel';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import path from 'path';
