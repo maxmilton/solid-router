722e093eca5232d1349cb80d1dc273dd48d5ce42
Max Milton <max@happyhackers.xyz>
2024-07-18 13:12:57 +0900

    chore: Tweak playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  testMatch: 'test/e2e/*.spec.ts',
+  testMatch: 'test/e2e/**/*.spec.ts',
+  forbidOnly: !!process.env.CI,
+  use: {
+    acceptDownloads: false,
+    contextOptions: { strictSelectors: true },
+    locale: 'en-US',
+    timezoneId: 'UTC',
+  },
 });
