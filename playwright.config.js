8623c9ad49f24d3ab7340f4a61b3e00e88ea4268
Max Milton <max@2okki.com>
2026-07-01 08:24:19 +1000

    chore: Refactor tooling configs

---
+++ playwright.config.js
@@ -0,0 +1,31 @@
+import { defineConfig, devices } from "@playwright/test";
+
+export default defineConfig({
+  testDir: "test/e2e",
+  testMatch: "test/e2e/**/*.spec.ts",
+  failOnFlakyTests: !!process.env.CI,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : 0,
+  use: {
+    acceptDownloads: false,
+    contextOptions: { strictSelectors: true },
+    locale: "en-US",
+    timezoneId: "UTC",
+    trace: "on-first-retry",
+  },
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"], channel: "chromium" },
+    },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    // FIXME: Broken in both CI and on local dev machines.
+    // {
+    //   name: "webkit",
+    //   use: { ...devices["Desktop Safari"] },
+    // },
+  ],
+});
